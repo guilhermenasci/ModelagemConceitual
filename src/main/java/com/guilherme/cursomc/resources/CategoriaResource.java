a78979fd84f163a1d8c874f3c60eefa0311b7848
Guilherme Nascimento <guinascimento.job@gmail.com>
2022-03-22 22:04:47 -0300

    Test Domain Class

--- src/main/java/com/guilherme/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/guilherme/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.guilherme.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.catalina.LifecycleState;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.guilherme.cursomc.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest working";
-	}
+	public List<Categoria> listar(){
+		
+		Categoria cat1 =  new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
 	
+		return lista;
+		
+	}
+
 }
